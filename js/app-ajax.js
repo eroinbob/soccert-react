82d56857e1baabc515d02e9676c5163abca2ed4c
eroinbob <boberoin@gmail.com>
2017-04-19 17:07:16 +0200

    add first div

--- js/app-ajax.js
+++ js/app-ajax.js
@@ -1,27 +1,37 @@
 var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "http://api.football-data.org/v1/soccerseasons/426/leagueTable",
-  "method": "GET",
-  "headers": {
-    "X-Auth-Token": "2ac0452cdead4db09a842c3a4f279114",
-  }
+    "async": true,
+    "crossDomain": true,
+    "url": "http://api.football-data.org/v1/soccerseasons/426/leagueTable",
+    "method": "GET",
+    "headers": {
+        "X-Auth-Token": "2ac0452cdead4db09a842c3a4f279114",
+    }
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+$.ajax(settings).done(function(response) {
+    console.log(response);
+    var teams = [];
+    var person = [];
+    for (var i = response.standing.length - 1; i >= 0; i--) {
+        teams.push(response.standing[i].teamName);
+    }
+    for (var i = teams.length - 1; i >= 0; i--) {
+        console.log(teams[i]);
+    }
 
-  var teams = [];
-  var person = [];
- 	for (var i = response.standing.length - 1; i >= 0; i--) {
- 		teams.push(response.standing[i].teamName);
- 	}
 
- 	for (var i = teams.length - 1; i >= 0; i--) {
- 		console.log(teams[i]);
- 	}
-   
+    var divEl = React.DOM.div({ className: "row" }, teams);
+
+    for (var i = teams.length - 1; i >= 0; i--) {
+        var el = React.createElement(
+            'div', { className: 'row'+i },
+            teams[i]
+        );
+
+    }
+
+
+     ReactDOM.render(el, document.getElementById("container"));
+
+
 });
-
-
-
